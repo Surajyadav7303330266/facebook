8d1a6d5bb60879897dd36faa57e3c9ec673cc708
Surajyadav7303330266 <sy9621551@gmail.com>
2019-12-19 12:32:23 +0530

    my second commit

---
+++ Assets/js/index.js
@@ -0,0 +1,204 @@
+// Uncomment to initialise WOW.js
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+});
+
+  var ukeVid1 = document.getElementById('uke1');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#uke1", duration: 200})
+        .addTo(controller)
+        //.addIndicators() // add indicators (requires plugin)
+
+        .on("enter", function () {
+          ukeVid1.play();
+        })
+        .on("leave", function () {
+          ukeVid1.pause();
+        })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Uncomment to initialise WOW.js
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+});
+
+  var ukeVid2 = document.getElementById('uke2');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#uke2", duration: 200})
+        .addTo(controller)
+        //.addIndicators() // add indicators (requires plugin)
+
+        .on("enter", function () {
+          ukeVid2.play();
+        })
+        .on("leave", function () {
+          ukeVid2.pause();
+        })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Uncomment to initialise WOW.js
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+});
+
+  var ukeVid3 = document.getElementById('uke3');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#uke3", duration: 200})
+        .addTo(controller)
+        //.addIndicators() // add indicators (requires plugin)
+
+        .on("enter", function () {
+          ukeVid3.play();
+        })
+        .on("leave", function () {
+          ukeVid3.pause();
+        })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Uncomment to initialise WOW.js
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+});
+
+  var ukeVid4 = document.getElementById('uke4');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#uke4", duration: 200})
+        .addTo(controller)
+        //.addIndicators() // add indicators (requires plugin)
+
+        .on("enter", function () {
+          ukeVid4.play();
+        })
+        .on("leave", function () {
+          ukeVid4.pause();
+        })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+});
+
+  var ukeVid5 = document.getElementById('uke5');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#uke5", duration: 200})
+        .addTo(controller)
+        //.addIndicators() // add indicators (requires plugin)
+
+        .on("enter", function () {
+          ukeVid5.play();
+        })
+        .on("leave", function () {
+          ukeVid5.pause();
+        })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+});
+
+  var ukeVid6 = document.getElementById('uke6');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#uke6", duration: 200})
+        .addTo(controller)
+        //.addIndicators() // add indicators (requires plugin)
+
+        .on("enter", function () {
+          ukeVid6.play();
+        })
+        .on("leave", function () {
+          ukeVid6.pause();
+        })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+});
+
+  var ukeVid7 = document.getElementById('uke7');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#uke7", duration: 200})
+        .addTo(controller)
+        //.addIndicators() // add indicators (requires plugin)
+
+        .on("enter", function () {
+          ukeVid7.play();
+        })
+        .on("leave", function () {
+          ukeVid7.pause();
+        })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+new WOW().init();
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true
+  });
+});
+
+  var ukeVid8 = document.getElementById('uke8');
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "#uke8", duration: 200})
+        .addTo(controller)
+        //.addIndicators() // add indicators (requires plugin)
+
+        .on("enter", function () {
+          ukeVid8.play();
+        })
+        .on("leave", function () {
+          ukeVid8.pause();
+        })
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
